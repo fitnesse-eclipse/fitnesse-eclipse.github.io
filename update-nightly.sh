6f0aad7278590e7d3bda77bb24fae46c150637c3
a1dutch <aholland@a1dutch.co.uk>
2014-12-12 23:04:41 +0000

    remove nightly script

--- update-nightly.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-git config user.email "fitnesseclipse@a1dutch.co.uk"
-git config user.name "fitnesseclipse"
-rm -rf nightly/*
-cp -r ~/fitnesse-eclipse/fitnesseclipse.site/target/repository/* nightly
-git commit --all -m "Update Nightly"
